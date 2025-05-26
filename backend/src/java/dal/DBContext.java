97f36fdc69e939d220da063e532a6e633fa3536d
hirO <137603363+phanhung09123@users.noreply.github.com>
2025-05-26 16:50:04 +0700

    Update file prep for intellij idea

--- backend/src/java/dal/DBContext.java
+++ backend/src/java/dal/DBContext.java
@@ -4,17 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DBContext {
     protected Connection connection;
-    public DBContext()
-    {
+
+    public DBContext() {
         try {
-            // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Artlanta;encrypt=true;trustServerCertificate=true";
-            String username = "sa";
-            String password = "123";
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:mysql://localhost:3306/Artlanta?useSSL=false&serverTimezone=UTC";
+            String username = "root";
+            String password = "12345";
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
