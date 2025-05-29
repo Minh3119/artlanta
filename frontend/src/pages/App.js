51cee6a213b2bf559a02d9ba72339abc6d4f61b4
Khaznh <141153460+Khaznh@users.noreply.github.com>
2025-05-29 11:54:18 +0700

    Add homepage

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -8,6 +8,9 @@ import UserProfilePage from "./UserProfile.js"
 import SessionTest from "../pages/SessionTest.js"
 import Artists from "./Artists.js"
 import CreatePostComponent from '../components/PostControl/createPostComponent.js';
+import HomePage from './HomePage.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 //import { ToastContainer } from 'react-toastify';
 
@@ -23,6 +26,7 @@ function App() {
           <Route path="/artists" element={<Artists />} />
           <Route path="/sessiontest" element={<SessionTest />} />
           <Route path="/createpost" element={<CreatePostComponent />} />
+          <Route path="/homepage" element={<HomePage/>} />
         </Routes>
       </BrowserRouter >
       <ToastContainer />
