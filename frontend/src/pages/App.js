efc1b190af6201792c4f654529153a1c53284db9
Minh3119 <65076982+Minh3119@users.noreply.github.com>
2025-06-02 00:23:43 +0700

    Removed unnecessary imports in App.js

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -3,7 +3,6 @@ import '../styles/App.scss';
 import '../styles/post.scss';
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserSettingComponent from '../components/UserProfileEdit/userSettingComponent.js';
 import UserProfilePage from "./UserProfile.js"
 import SessionTest from "../pages/SessionTest.js"
 import Artists from "./Artists.js"
@@ -11,19 +10,12 @@ import CreatePostComponent from '../components/PostControl/createPostComponent.j
 import HomePage from './HomePage.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import EditProfileComponent from '../components/UserProfileEdit/editProfileComponent.js';
-import EditPasswordComponent from '../components/UserProfileEdit/editPasswordComponent.js';
-import EditNotificationComponent from '../components/UserProfileEdit/editNotificationComponent.js';
-import EditPricingComponent from '../components/UserProfileEdit/editPricingComponent.js';
-import DeleteAccountComponent from '../components/UserProfileEdit/deleteAccountComponent.js';
 import UpdatePostComponent from '../components/PostControl/updatePostComponent.js';
 import DeletePostComponent from '../components/PostControl/deletePostComponent.js';
 import Login from './Login.js';
 import Register from './Register.js';
 import PassForget from './PassForget.js';
-
 import PostListPage from "./Post.js"
-//import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
