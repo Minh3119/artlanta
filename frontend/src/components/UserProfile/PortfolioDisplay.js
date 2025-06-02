f1ce5d476c468cb83ccec2ba16c318f85a770268
Minh3119 <65076982+Minh3119@users.noreply.github.com>
2025-06-03 00:43:28 +0700

    Fixed Cancel button for Edit Portfolio

--- frontend/src/components/UserProfile/PortfolioDisplay.js
+++ frontend/src/components/UserProfile/PortfolioDisplay.js
@@ -15,7 +15,7 @@ const PortfolioDisplay = ({
 
     return (
         <div className="relative">
-            <div className="relative rounded-3xl overflow-hidden shadow-xl aspect-square">
+            <div className="relative rounded-3xl overflow-auto shadow-xl aspect-square">
                 <img
                     className="w-full h-full object-cover"
                     src={currentImage.url}
